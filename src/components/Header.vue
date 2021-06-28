bc96e47504f1332421e1b35c77f9064b7430c32a
Fakhrizal <farzalanan_ini@yahoo.co.id>
2021-06-28 12:27:15 +0700

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,7 +12,6 @@
             </div>
             <nav class="nav right">
                 <g-link class="nav__link" to="/journal">Journal</g-link>
-                <g-link class="nav__link" to="/contact">Say Hi!</g-link>
             </nav>
         </div>
     </header>
